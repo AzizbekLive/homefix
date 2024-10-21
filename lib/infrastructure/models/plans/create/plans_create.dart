662709dc13bb9e4d4c83e185df5771ecd8e709d5
azizbek.live <otaboevlive@gmail.com>
2024-10-21 16:39:05 +0500

    change: "plans" model

--- lib/infrastructure/models/plans/create/plans_create.dart
+++ lib/infrastructure/models/plans/create/plans_create.dart
@@ -1,11 +1,13 @@
 class PlansCreate {
+  int? id;
   String? name;
   DateTime? date;
   bool? status;
 
-  PlansCreate({this.name, this.date, this.status});
+  PlansCreate({this.id, this.name, this.date, this.status});
 
   PlansCreate.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
     name = json['name'];
     date = json['date'] != null ? DateTime.parse(json['date']) : null;
     status = json['status'];
@@ -13,6 +15,7 @@ class PlansCreate {
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
     data['name'] = name;
     data['date'] = date?.toIso8601String();
     data['status'] = status;
