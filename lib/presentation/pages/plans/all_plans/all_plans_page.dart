2107a1702ae6a4a64c874063cfbb2ab2525c97b5
azizbek.live <otaboevlive@gmail.com>
2024-10-21 19:40:18 +0500

    add: plans list item

--- lib/presentation/pages/plans/all_plans/all_plans_page.dart
+++ lib/presentation/pages/plans/all_plans/all_plans_page.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:homefix/presentation/pages/plans/all_plans/widgets/notes_item.dart';
+
+import '../../../styles/path/app_resources.dart';
+import '../../../styles/theme/app_theme.dart';
 
 class AllPlansPage extends StatelessWidget {
   const AllPlansPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          'My Plans List',
+          style: AppTheme.headlineMedium,
+        ),
+        actions: [
+          InkWell(
+              child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SvgPicture.asset(AppResources.svgAdd),
+          )),
+          const SizedBox(width: 14)
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/presentation/pages/plans/all_plans/widgets/notes_item.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:homefix/presentation/styles/path/app_resources.dart';
+
+import '../../../../styles/colors/app_colors.dart';
+
+class NotesItem extends StatefulWidget {
+  final String title;
+  final String date;
+  final bool initialStatus;
+
+  const NotesItem({
+    super.key,
+    required this.title,
+    required this.date,
+    required this.initialStatus,
+  });
+
+  @override
+  _NotesItemState createState() => _NotesItemState();
+}
+
+class _NotesItemState extends State<NotesItem> {
+  late bool _status;
+
+  @override
+  void initState() {
+    super.initState();
+    _status = widget.initialStatus;
+  }
+
+  void _onChanged(bool? newValue) {
+    setState(() {
+      _status = newValue ?? false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: AppColors.grayEE, borderRadius: BorderRadius.circular(8)),
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: Row(
+        children: [
+          GestureDetector(
+            onTap: () => _onChanged(!_status),
+            child: Container(
+              width: 30.0,
+              height: 30.0,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: _status ? Colors.transparent : Colors.transparent,
+                border: Border.all(
+                  color: _status ? AppColors.lightGreen : Colors.grey,
+                  width: 3.0,
+                ),
+              ),
+              child: _status
+                  ? Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: SvgPicture.asset(AppResources.svgTick),
+                    )
+                  : null,
+            ),
+          ),
+          const SizedBox(width: 8.0),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.title,
+                  style: const TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  widget.date,
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.grey[600],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
