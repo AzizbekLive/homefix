6722aa5e7f1d60bb90f2429f618f0661efaf6b7c
azizbek.live <otaboevlive@gmail.com>
2024-10-21 19:06:31 +0500

    add: styles added

---
+++ lib/presentation/styles/colors/app_colors.dart
@@ -0,0 +1,11 @@
+
+import 'dart:ui';
+
+class AppColors{
+  static Color lightGreen = const Color(0xFF26BDBE);
+  static Color dark = const Color(0xFF2C2C2C);
+  static Color black = const Color(0xFF000000);
+  static Color gray = const Color(0xFF787878);
+  static Color gray65 = const Color(0xFF656565);
+  static Color white = const Color(0xFFE7E7E7);
+}
---
+++ lib/presentation/styles/path/app_resources.dart
@@ -0,0 +1,13 @@
+class AppResources {
+
+  //Icons folder details SVG
+  static String svgHome24 = "assets/svg/home_24.svg";
+  static String svgHome36 = "assets/svg/home_36.svg";
+  static String svgDate24 = "assets/svg/date_24.svg";
+  static String svgDate36 = "assets/svg/date_36.svg";
+  static String svgChat24 = "assets/svg/chat_24.svg";
+  static String svgChat36 = "assets/svg/chat_36.svg";
+  static String svgProfile24 = "assets/svg/profile_24.svg";
+  static String svgProfile36 = "assets/svg/profile_36.svg";
+
+}
